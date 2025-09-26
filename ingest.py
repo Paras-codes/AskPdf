9448a71ed76ecc073dc3d378506a785d3d759558
Paras-Codes <parassahu06@gmail.com>
2025-09-26 16:57:23 +0530

    ingest added

---
+++ ingest.py
@@ -0,0 +1,35 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
+from langchain_community.vectorstores import Chroma
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import os
+from config import PERSIST_DIR, CHUNK_SIZE, CHUNK_OVERLAP, EMBEDDING_MODEL
+
+def ingest_pdfs(file_paths: list[str], persist_dir=PERSIST_DIR):
+    embeddings = HuggingFaceEndpointEmbeddings(
+    repo_id="sentence-transformers/all-MiniLM-L6-v2",
+    task="feature-extraction"
+)
+    vectordb = Chroma(persist_directory=persist_dir, embedding_function=embeddings)
+
+    total_chunks = 0
+    for file_path in file_paths:
+        filename = os.path.basename(file_path)
+        loader = PyPDFLoader(file_path)
+        docs = loader.load()
+
+        splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
+        chunks = splitter.split_documents(docs)
+
+        for i, chunk in enumerate(chunks):
+            chunk.metadata["doc_id"] = f"{filename}_chunk_{i}"
+
+        vectordb.add_documents(chunks)
+        total_chunks += len(chunks)
+
+    vectordb.persist()
+    return {"status": "success", "total_chunks": total_chunks}
